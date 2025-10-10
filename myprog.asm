b890ceec16259ba36412f30170e9f9b8c21dade3
Arno Noomen <arnonoomen@live.nl>
2025-10-10 11:23:36 +0200

    251010b

--- myprog.asm
+++ myprog.asm
@@ -3,7 +3,7 @@ MYPROG   CSECT
          LR    12,15          Establish
          USING MYPROG,12         addressability
 *  Save pointer to calling programs registers
-         ST    13,SAVE+4
+         ST    13,13,SAVE+4
 *  Point to own save area
          LA    13,SAVE
 **********************************************
@@ -20,4 +20,20 @@ MYPROG   CSECT
 *
 ***********************************************
 SAVE     DC    18F'0'
+R0       EQU   0
+R1       EQU   1
+R2       EQU   2
+R3       EQU   3
+R4       EQU   4
+R5       EQU   5
+R6       EQU   6
+R7       EQU   7
+R8       EQU   8
+R9       EQU   9
+R10      EQU   10
+R11      EQU   11
+R12      EQU   12
+R13      EQU   13
+R14      EQU   14
+R15      EQU   15
          END   MYPROG
